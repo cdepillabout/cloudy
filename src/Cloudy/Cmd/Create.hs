7c55923dfa045e92399157cd67491890da4656d7
(cdep)illabout <cdep.illabout@gmail.com>
2024-08-09 13:28:42 +0900

    Get first crack at accessing scaleway working

--- src/Cloudy/Cmd/Create.hs
+++ src/Cloudy/Cmd/Create.hs
@@ -1,8 +1,13 @@
-
 module Cloudy.Cmd.Create where
 
 import Cloudy.Cli (CreateCliOpts (..), AwsCliOpts (..), ScalewayCliOpts (..))
-import Cloudy.Scaleway (ipsPostApi)
+import Cloudy.Scaleway (ipsPostApi, Zone (..), IpsReq (..), ProjectId (..))
+import Control.Monad.IO.Class (liftIO)
+import Data.Text (Text)
+import Network.HTTP.Client.TLS (newTlsManager)
+import Servant.Client (mkClientEnv, BaseUrl (BaseUrl), Scheme (Https), runClientM, ClientM)
+import Servant.Client.Core (mkAuthenticatedRequest, AuthenticatedRequest, AuthClientData, Request, addHeader)
+import Servant.API (AuthProtect)
 
 runCreate :: CreateCliOpts -> IO ()
 runCreate = \case
@@ -12,5 +17,25 @@ runCreate = \case
 runCreateAws :: AwsCliOpts -> IO ()
 runCreateAws awsOpts = undefined
 
+
 runCreateScaleway :: ScalewayCliOpts -> IO ()
-runCreateScaleway scalewayOpts = undefined
+runCreateScaleway scalewayOpts = do
+  manager <- newTlsManager
+  let clientEnv = mkClientEnv manager scalewayBaseUrl
+  res <- runClientM go clientEnv
+  print res
+  where
+    go :: ClientM ()
+    go = do
+      let authData = "testtesttest"
+      let authReq = mkAuthenticatedRequest authData blahblah :: AuthenticatedRequest (AuthProtect "auth-token")
+      res <- ipsPostApi authReq NL1 (IpsReq "routed_ipv4" $ ProjectId "62265224-89aa-4d04-b129-6b405569fdea")
+      liftIO $ putStrLn $ "ips res: " <> show res
+
+blahblah :: Text -> Request -> Request
+blahblah authData = addHeader "X-Auth-Token" authData
+
+type instance AuthClientData (AuthProtect "auth-token") = Text
+
+scalewayBaseUrl :: BaseUrl
+scalewayBaseUrl = BaseUrl Https "api.scaleway.com" 443 ""
--- src/Cloudy/Scaleway.hs
+++ src/Cloudy/Scaleway.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 
 module Cloudy.Scaleway where
 
+import Data.Aeson (ToJSON(..), object, (.=), FromJSON (..), withObject, Value, (.:), withText)
 import Data.Map.Strict (Map)
 import Data.Proxy (Proxy (Proxy))
 import Data.Text (Text)
 import Servant.API ((:>), Capture, ReqBody, JSON, Post, AuthProtect, (:<|>) ((:<|>)))
 import Servant.Client (client, ClientM)
 import Servant.Client.Core (AuthenticatedRequest)
-import Web.HttpApiData (ToHttpApiData (..))
+import Web.HttpApiData (ToHttpApiData (..), FromHttpApiData)
+import Data.Aeson.Types (Parser)
 
 
 -- curl 'https://api.scaleway.com/instance/v1/zones/nl-ams-1/servers'\
@@ -24,31 +27,74 @@ import Web.HttpApiData (ToHttpApiData (..))
 data Zone = NL1 | NL2 | NL3
   deriving (Eq, Show)
 
+instance ToJSON Zone where toJSON = toJSON . zoneToText
+instance FromJSON Zone where
+  parseJSON = withText "Zone" $ maybe (fail "Failed to parse Zone") pure . zoneFromText
+
 zoneToText :: Zone -> Text
 zoneToText = \case
   NL1 -> "nl-ams-1"
   NL2 -> "nl-ams-2"
   NL3 -> "nl-ams-3"
 
+zoneFromText :: Text -> Maybe Zone
+zoneFromText = \case
+  "nl-ams-1" -> Just NL1
+  "nl-ams-2" -> Just NL2
+  "nl-ams-3" -> Just NL3
+  _ -> Nothing
+
+
 instance ToHttpApiData Zone where
   toUrlPiece :: Zone -> Text
   toUrlPiece = zoneToText
 
 newtype ImageId = ImageId { unImageId :: Text }
+  deriving stock (Eq, Show)
+  deriving newtype (FromHttpApiData, FromJSON, ToHttpApiData, ToJSON)
+
 newtype IpId = IpId { unIpId :: Text }
+  deriving stock (Eq, Show)
+  deriving newtype (FromHttpApiData, FromJSON, ToHttpApiData, ToJSON)
+
 newtype OrganizationId = OrganizationId { unOrganizationId :: Text }
+  deriving stock (Eq, Show)
+  deriving newtype (FromHttpApiData, FromJSON, ToHttpApiData, ToJSON)
+
 newtype ProjectId = ProjectId { unProjectId :: Text }
+  deriving stock (Eq, Show)
+  deriving newtype (FromHttpApiData, FromJSON, ToHttpApiData, ToJSON)
+
 newtype ServerId = ServerId { unServerId :: Text }
+  deriving stock (Eq, Show)
+  deriving newtype (FromHttpApiData, FromJSON, ToHttpApiData, ToJSON)
 
 data Volume = Volume
   { size :: Int
   , volumeType :: Text
   }
+  deriving stock Show
+
+instance ToJSON Volume where
+  toJSON volume =
+    object
+      [ "size" .= volume.size
+      , "volume_type" .= volume.volumeType
+      ]
+
 
 data IpsReq = IpsReq
   { type_ :: Text
   , project :: ProjectId
   }
+  deriving stock Show
+
+instance ToJSON IpsReq where
+  toJSON ipsReq =
+    object
+      [ "type" .= ipsReq.type_
+      , "project" .= ipsReq.project
+      ]
 
 data IpsResp = IpsResp
   { id :: IpId
@@ -57,6 +103,18 @@ data IpsResp = IpsResp
   , project :: ProjectId
   , zone :: Zone
   }
+  deriving stock Show
+
+instance FromJSON IpsResp where
+  parseJSON :: Value -> Parser IpsResp
+  parseJSON = withObject "IpsResp outer wrapper" $ \o -> do
+    innerObj <- o .: "ips"
+    id_ <- innerObj .: "id"
+    address <- innerObj .: "address"
+    organization <- innerObj .: "organization"
+    project <- innerObj .: "project"
+    zone <- innerObj .: "zone"
+    pure IpsResp { id = id_, address, organization, project, zone }
 
 data ServersReq = ServersReq
   { bootType :: Text
@@ -68,14 +126,40 @@ data ServersReq = ServersReq
   , volumes :: Map Text Volume
   , project :: ProjectId
   }
+  deriving stock Show
+
+instance ToJSON ServersReq where
+  toJSON serversReq =
+    object
+      [ "boot_type" .= serversReq.bootType
+      , "commercial_type" .= serversReq.commercialType
+      , "image" .= serversReq.image
+      , "name" .= serversReq.name
+      , "public_ips" .= serversReq.publicIps
+      , "tags" .= serversReq.tags
+      , "volumes" .= serversReq.volumes
+      , "project" .= serversReq.project
+      ]
 
 data ServersResp = ServersResp
   { id :: ServerId
   , name :: Text
   }
+  deriving stock Show
 
-type InstanceIpsPostApi = AuthProtect "auth-token" :> "instance" :> "v1" :> "zones" :> Capture "zone" Zone :> "ips" :> ReqBody '[JSON] IpsReq :> Post '[JSON] IpsResp
-type InstanceServersPostApi = AuthProtect "auth-token" :> "instance" :> "v1" :> "zones" :> Capture "zone" Zone :> "servers" :> ReqBody '[JSON] ServersReq :> Post '[JSON] ServersResp
+instance FromJSON ServersResp where
+  parseJSON :: Value -> Parser ServersResp
+  parseJSON = withObject "ServersResp outer wrapper" $ \o -> do
+    innerObj <- o .: "servers"
+    id_ <- innerObj .: "id"
+    name <- innerObj .: "name"
+    pure ServersResp { id = id_, name }
+
+type InstanceIpsPostApi =
+  AuthProtect "auth-token" :> "instance" :> "v1" :> "zones" :> Capture "zone" Zone :> "ips" :> ReqBody '[JSON] IpsReq :> Post '[JSON] IpsResp
+
+type InstanceServersPostApi =
+  AuthProtect "auth-token" :> "instance" :> "v1" :> "zones" :> Capture "zone" Zone :> "servers" :> ReqBody '[JSON] ServersReq :> Post '[JSON] ServersResp
 
 type ScalewayApi = InstanceIpsPostApi :<|> InstanceServersPostApi
 
