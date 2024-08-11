b3b956be06109dcb413b31ad5a1d3bbd5254aead
(cdep)illabout <cdep.illabout@gmail.com>
2024-08-12 08:06:52 +0900

    update

--- src/Cloudy/Cmd/Scaleway.hs
+++ src/Cloudy/Cmd/Scaleway.hs
@@ -2,18 +2,9 @@
 module Cloudy.Cmd.Scaleway where
 
 import Cloudy.Cli (ScalewayCliOpts (..))
-import Cloudy.Cli.Scaleway (ScalewayCreateCliOpts (..), ScalewayListInstanceTypesCliOpts (..))
-import Cloudy.Cmd.Scaleway.Create
-import Cloudy.Cmd.Scaleway.ListInstanceTypes
-import Cloudy.LocalConfFile (LocalConfFileOpts (..), LocalConfFileScalewayOpts (..))
-import Cloudy.NameGen (instanceNameGen)
-import Cloudy.Scaleway (ipsPostApi, Zone (..), IpsReq (..), ProjectId (..), zoneFromText, serversPostApi, ServersReq (..))
-import Control.Monad.IO.Class (liftIO)
-import Data.Text (Text, unpack)
-import Network.HTTP.Client.TLS (newTlsManager)
-import Servant.API (AuthProtect)
-import Servant.Client (mkClientEnv, BaseUrl (BaseUrl), Scheme (Https), runClientM, ClientM)
-import Servant.Client.Core (mkAuthenticatedRequest, AuthenticatedRequest, AuthClientData, Request, addHeader)
+import Cloudy.Cmd.Scaleway.Create (runCreate)
+import Cloudy.Cmd.Scaleway.ListInstanceTypes (runListInstanceTypes)
+import Cloudy.LocalConfFile (LocalConfFileOpts)
 
 runScaleway :: LocalConfFileOpts -> ScalewayCliOpts -> IO ()
 runScaleway localConfFileOpts = \case
