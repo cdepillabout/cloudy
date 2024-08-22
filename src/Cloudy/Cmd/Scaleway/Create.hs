5972ae71f4e9ed8741e2fa7b0b702a95b29f74f2
(cdep)illabout <cdep.illabout@gmail.com>
2024-08-22 14:25:22 +0900

    wip

--- src/Cloudy/Cmd/Scaleway/Create.hs
+++ src/Cloudy/Cmd/Scaleway/Create.hs
@@ -5,11 +5,12 @@ module Cloudy.Cmd.Scaleway.Create where
 import Cloudy.Cli.Scaleway (ScalewayCreateCliOpts (..))
 import Cloudy.Cmd.Scaleway.Utils (createAuthReq, getZone, runScalewayClientM, getInstanceType, getImageId)
 import Cloudy.LocalConfFile (LocalConfFileOpts (..), LocalConfFileScalewayOpts (..))
-import Cloudy.NameGen (instanceNameGen)
-import Cloudy.Scaleway (ipsPostApi, Zone (..), IpsReq (..), IpsResp (..), ProjectId (..), serversPostApi, ServersReq (..), ServersResp (..), ImageId (ImageId), serversUserDataPatchApi, UserDataKey (UserDataKey), UserData (UserData), ServersActionReq (..), serversActionPostApi, ServersRespVolume (..), ServersReqVolume (..), VolumesReq (..), volumesPatchApi)
+import Cloudy.Db (newCloudyInstance, newScalewayInstance, withCloudyDb)
+import Cloudy.Scaleway (ipsPostApi, Zone (..), IpsReq (..), IpsResp (..), ProjectId (..), serversPostApi, ServersReq (..), ServersResp (..), ImageId (ImageId), serversUserDataPatchApi, UserDataKey (UserDataKey), UserData (UserData), ServersActionReq (..), serversActionPostApi, ServersRespVolume (..), ServersReqVolume (..), VolumesReq (..), volumesPatchApi, ServerId, unServerId)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Map.Strict as Map
 import Data.Text (Text, pack)
+import Data.Time (getCurrentTime)
 import Servant.Client (ClientM)
 import Servant.API (NoContent(NoContent))
 
@@ -50,64 +51,68 @@ mkSettings localConfFileOpts cliOpts = do
 runCreate :: LocalConfFileOpts -> ScalewayCreateCliOpts -> IO ()
 runCreate localConfFileOpts scalewayOpts = do
   settings <- mkSettings localConfFileOpts scalewayOpts
-  runScalewayClientM
-    (\err -> error $ "ERROR! Problem creating image: " <> show err)
-    (go settings)
-  where
-    go :: ScalewayCreateSettings -> ClientM ()
-    go settings = do
-      let authReq = createAuthReq settings.secretKey
-      ipsRes <- ipsPostApi authReq settings.zone (IpsReq "routed_ipv4" settings.projectId)
-      liftIO $ putStrLn $ "ips resp: " <> show ipsRes
-      serverName <- liftIO instanceNameGen
-      let serversReq =
-            ServersReq
-              { bootType = "local"
-              , commercialType = settings.instanceType
-              , image = ImageId settings.imageId
-              , name = serverName
-              , publicIps = [ipsRes.id]
-              , tags = ["cloudy"]
-              , volumes =
-                  Map.fromList
-                    [ ( "0"
-                      , ServersReqVolume
-                          { size = settings.volumeSizeGb * oneGb
-                          , volumeType = "b_ssd"
-                          }
-                      )
-                    ]
-              , project = settings.projectId
-              }
-      serversResp <- serversPostApi authReq settings.zone serversReq
-      liftIO $ putStrLn $ "servers resp: " <> show serversResp
-      let maybeFirstVol = Map.lookup "0" serversResp.volumes
-      firstVol <- maybe (error "couldn't find first volume, unexpected") pure maybeFirstVol
-      -- The volume's name has to initial be created as empty (""), and only
-      -- after that can we set the name separately.
-      serversVolumesResp <-
-        volumesPatchApi
-          authReq
-          settings.zone
-          firstVol.id
-          (VolumesReq $ serverName <> "-boot-block-volume")
-      liftIO $ putStrLn $ "servers volumes resp: " <> show serversVolumesResp
-      let userData =
-            unlines
-              [ "#!/usr/bin/env bash"
-              , "echo 'hello' >> /whatwhat"
-              ]
-      NoContent <-
-        serversUserDataPatchApi
-          authReq
-          settings.zone
-          serversResp.id
-          (UserDataKey "cloud-init")
-          (UserData $ pack userData)
-      liftIO $ putStrLn "created user data"
-      let act = ServersActionReq { action = "poweron" }
-      task <- serversActionPostApi authReq settings.zone serversResp.id act
-      liftIO $ print task
+  withCloudyDb $ \conn -> do
+    (cloudyInstanceId, instanceName) <- newCloudyInstance conn
+    currentTime <- getCurrentTime
+    scalewayServerId <- runScalewayClientM
+      (\err -> error $ "ERROR! Problem creating image: " <> show err)
+      (createScalewayServer settings instanceName)
+    newScalewayInstance conn currentTime cloudyInstanceId (unServerId scalewayServerId)
+
+createScalewayServer :: ScalewayCreateSettings -> Text -> ClientM ServerId
+createScalewayServer settings instanceName = do
+  let authReq = createAuthReq settings.secretKey
+  ipsRes <- ipsPostApi authReq settings.zone (IpsReq "routed_ipv4" settings.projectId)
+  liftIO $ putStrLn $ "ips resp: " <> show ipsRes
+  let serversReq =
+        ServersReq
+          { bootType = "local"
+          , commercialType = "cloudy-" <> settings.instanceType
+          , image = ImageId settings.imageId
+          , name = instanceName
+          , publicIps = [ipsRes.id]
+          , tags = ["cloudy"]
+          , volumes =
+              Map.fromList
+                [ ( "0"
+                  , ServersReqVolume
+                      { size = settings.volumeSizeGb * oneGb
+                      , volumeType = "b_ssd"
+                      }
+                  )
+                ]
+          , project = settings.projectId
+          }
+  serversResp <- serversPostApi authReq settings.zone serversReq
+  liftIO $ putStrLn $ "servers resp: " <> show serversResp
+  let maybeFirstVol = Map.lookup "0" serversResp.volumes
+  firstVol <- maybe (error "couldn't find first volume, unexpected") pure maybeFirstVol
+  -- The volume's name has to initial be created as empty (""), and only
+  -- after that can we set the name separately.
+  serversVolumesResp <-
+    volumesPatchApi
+      authReq
+      settings.zone
+      firstVol.id
+      (VolumesReq $ "cloudy-" <> instanceName <> "-boot-block-volume")
+  liftIO $ putStrLn $ "servers volumes resp: " <> show serversVolumesResp
+  let userData =
+        unlines
+          [ "#!/usr/bin/env bash"
+          , "echo 'hello' >> /whatwhat"
+          ]
+  NoContent <-
+    serversUserDataPatchApi
+      authReq
+      settings.zone
+      serversResp.id
+      (UserDataKey "cloud-init")
+      (UserData $ pack userData)
+  liftIO $ putStrLn "created user data"
+  let act = ServersActionReq { action = "poweron" }
+  task <- serversActionPostApi authReq settings.zone serversResp.id act
+  liftIO $ print task
+  pure serversResp.id
 
 oneGb :: Int
 oneGb = 1000 * 1000 * 1000
--- src/Cloudy/Db.hs
+++ src/Cloudy/Db.hs
@@ -2,9 +2,16 @@
 
 module Cloudy.Db where
 
-import Database.SQLite.Simple (withConnection, Connection, execute_, Query, query_, FromRow)
+import Cloudy.NameGen (instanceNameGen)
 import Cloudy.Path (getCloudyDbPath)
 import Control.Exception (Exception, throwIO)
+import Data.Int (Int64)
+import Data.Maybe (listToMaybe)
+import Data.Text (Text)
+import Data.Time (UTCTime)
+import Database.SQLite.Simple (withConnection, Connection, execute_, Query, query_, FromRow (..), ToRow (..), execute, withTransaction, lastInsertRowId, query, Only (..), field)
+import Database.SQLite.Simple.FromField (FromField)
+import Database.SQLite.Simple.ToField (ToField)
 
 createLocalDatabase :: Connection -> IO ()
 createLocalDatabase conn = do
@@ -12,14 +19,14 @@ createLocalDatabase conn = do
     conn
     "CREATE TABLE IF NOT EXISTS cloudy_instance \
     \  ( id INTEGER PRIMARY KEY AUTOINCREMENT \
-    \  , name TEXT NOT NULL \
-    \  , created_at DATETIME NOT NULL DEFAULT (datetime('now', 'utc')) \
+    \  , name TEXT NOT NULL UNIQUE \
+    \  , created_at DATETIME \
     \  , deleted_at DATETIME \
     \  ) \
     \STRICT; \
     \\
     \CREATE TABLE IF NOT EXISTS scaleway_instance \
-    \  ( cloudy_instance_id INTEGER NOT NULL \
+    \  ( cloudy_instance_id INTEGER NOT NULL UNIQUE \
     \  , scaleway_instance_id TEXT NOT NULL UNIQUE \
     \  , FOREIGN KEY (cloudy_instance_id) REFERENCES cloudy_instance(id) \
     \  ) \
@@ -28,7 +35,9 @@ createLocalDatabase conn = do
 withCloudyDb :: (Connection -> IO ()) -> IO ()
 withCloudyDb action = do
   dcutDbPath <- getCloudyDbPath
-  withSqliteConn dcutDbPath action
+  withSqliteConn dcutDbPath $ \conn -> do
+    createLocalDatabase conn
+    action conn
 
 withSqliteConn :: FilePath -> (Connection -> IO ()) -> IO ()
 withSqliteConn dbPath action =
@@ -66,3 +75,120 @@ querySingle_ conn q = do
     [] -> throwIO $ QuerySingleErr q "query returned NO results, expecting exactly one"
     [a] -> pure a
     _:_ -> throwIO $ QuerySingleErr q "query returned multiple results, expecting exactly one"
+
+newtype CloudyInstanceId = CloudyInstanceId { unCloudyInstanceId :: Int64 }
+  deriving stock (Eq, Show)
+  deriving newtype (FromField, ToField)
+
+data CloudyInstance = CloudyInstance
+  { id :: CloudyInstanceId
+  , name :: Text
+  , createdAt :: Maybe UTCTime
+  , deletedAt :: Maybe UTCTime
+  }
+  deriving stock (Eq, Show)
+
+instance FromRow CloudyInstance where
+  fromRow = do
+    id' <- field
+    name <- field
+    createdAt <- field
+    deletedAt <- field
+    pure $ CloudyInstance { id = id', name, createdAt, deletedAt }
+
+data ScalewayInstance = ScalewayInstance
+  { cloudyInstanceId :: CloudyInstanceId
+  , scalewayInstanceId :: Text
+  }
+  deriving stock (Eq, Show)
+
+instance FromRow ScalewayInstance where
+  fromRow = do
+    cloudyInstanceId <- field
+    scalewayInstanceId <- field
+    pure $ ScalewayInstance { cloudyInstanceId, scalewayInstanceId }
+
+instance ToRow ScalewayInstance where
+  toRow ScalewayInstance {cloudyInstanceId, scalewayInstanceId} =
+    toRow (cloudyInstanceId, scalewayInstanceId)
+
+newCloudyInstance :: Connection -> IO (CloudyInstanceId, Text)
+newCloudyInstance conn = withTransaction conn go
+  where
+    go :: IO (CloudyInstanceId, Text)
+    go = do
+      possibleName <- instanceNameGen
+      maybeInstance <- findCloudyInstanceByName conn possibleName
+      case maybeInstance of
+        -- No instance exists with this name yet. Insert a new blank instance.
+        Nothing -> do
+          execute
+            conn
+            "INSERT INTO cloudy_instance \
+            \(name) \
+            \VALUES (?)"
+            (Only possibleName)
+          cloudyInstanceId <- lastInsertRowId conn
+          pure (CloudyInstanceId cloudyInstanceId, possibleName)
+        -- An instance already exists with this name, try again.
+        Just _ -> go
+
+findCloudyInstanceByName :: Connection -> Text -> IO (Maybe CloudyInstance)
+findCloudyInstanceByName conn cloudyInstanceName = do
+  listToMaybe <$>
+    query
+      conn
+      "SELECT id, name, created_at, deleted_at \
+      \FROM cloudy_instance \
+      \WHERE name == ? \
+      \ORDER BY id"
+      (Only cloudyInstanceName)
+
+newScalewayInstance ::
+  Connection -> UTCTime -> CloudyInstanceId -> Text -> IO ()
+newScalewayInstance conn creationTime cloudyInstanceId scalewayInstanceId =
+  withTransaction conn $ do
+    execute
+      conn
+      "UPDATE cloudy_instance \
+      \SET created_at = ? \
+      \WHERE id = ?"
+      (creationTime, cloudyInstanceId)
+    execute
+      conn
+      "INSERT INTO scaleway_instance \
+      \(cloudy_instance_id, scaleway_instance_id) \
+      \VALUES (?, ?)"
+      ScalewayInstance { cloudyInstanceId, scalewayInstanceId }
+
+-- insertScalewayServer :: Connection -> Scaleway.ServerId -> Text -> IO ()
+-- insertScalewayServer conn scalewayServerId serverName = withTransaction $ do
+--   execute
+--     conn
+--     "INSERT INTO cloudy_instance \
+--     \(name) \
+--     \VALUES (?)"
+--     serverName
+
+-- insertLocalObservation :: Connection -> LocalObservation -> IO ()
+-- insertLocalObservation conn =
+--   execute
+--     conn
+--     "INSERT INTO idle_times \
+--     \(client_uuid, timestamp, idle_time_milli, hostname) \
+--     \VALUES (?,?,?,?)"
+
+-- insertNewObsFromServer :: Connection -> [ObsFromServer] -> IO ()
+-- insertNewObsFromServer conn obsFromServer =
+--   executeMany
+--     conn
+--     -- TODO: is ignoring a conflict the best thing to do here???
+--     -- Or maybe I need to replace it?
+--     -- It looks like SQLite has upsert functionality as well??
+--     -- https://www.sqlite.org/lang_upsert.html
+--     "INSERT OR IGNORE INTO idle_times \
+--     \(server_id, client_uuid, timestamp, idle_time_milli, hostname) \
+--     \VALUES (?,?,?,?,?)"
+--     (fmap toDb obsFromServer)
+--   where
+--     toDb (ObsFromServer servId uuid ts idle host) = (servId, UUID.toText uuid, ts, idle, host)
