8db213fbc337e382bae84a14d41093de15a01953
(cdep)illabout <cdep.illabout@gmail.com>
2024-09-13 14:09:13 +0900

    update

--- src/Cloudy/InstanceSetup.hs
+++ src/Cloudy/InstanceSetup.hs
@@ -1,4 +1,24 @@
 
 module Cloudy.InstanceSetup where
 
+import Data.Aeson (FromJSON(..), ToJSON (..), object, (.=), FromJSON, withObject, (.:))
+import Data.Text (Text)
 
+
+data InstanceSetup = InstanceSetup
+  { shortDescription :: Text
+  , cloudInitUserData :: Text
+  }
+
+instance ToJSON InstanceSetup where
+  toJSON InstanceSetup{shortDescription, cloudInitUserData} =
+    object
+      [ "short-description" .= shortDescription
+      , "cloud-init-user-data" .= cloudInitUserData
+      ]
+
+instance FromJSON InstanceSetup where
+  parseJSON = withObject "InstanceSetup" $ \o -> do
+    shortDescription <- o .: "short-description"
+    cloudInitUserData <- o .: "cloud-init-user-data"
+    pure InstanceSetup { shortDescription, cloudInitUserData }
